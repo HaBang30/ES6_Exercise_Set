6a7b8e216bd4d71aa5b65a95a5784e0cd2b3117f
HaBang30 <doanduytung1602@gmail.com>
2023-04-22 10:38:03 +0700

    es16Set

---
+++ index.js
@@ -0,0 +1,58 @@
+// using Set to manipulate data
+
+
+// let zoo = new Set();
+//add a new element to the Set === set.add(element).
+// zoo.add("tiger");
+// console.log(zoo.has("tiger"));
+// zoo.add("lions");
+// zoo.add("monkeys");
+// zoo.add("dog");
+// zoo.add("cat");
+// zoo.add('pig');
+// zoo.add("lions");
+// console.log(zoo);
+
+// detete an element form a set == set.delete(element);
+// zoo.delete("lions");
+
+// Has(): Return true if a value exists == set.has(element);
+// console.log(zoo.has("tiger"));
+
+// Clear(): removes all element from a set === set.clear()
+// zoo.clear();
+// console.log(zoo)
+
+// Lambda Expression: use forEach invokes a callback for each element in set:
+
+// values()::
+//  use  for...of (iterator) to take values from array set.
+// Return an iterator with all the values in a set() ===(set.values())
+// let str = new Set ([
+//     "a","b","c", "d"
+// ]);
+// for (const item of str.values()) {
+//     console.log(item);
+// }
+
+// Keys():
+// Same as values
+// for (const item of str.keys()) {
+//     console.log(item);
+// }
+
+
+// Entries(): returns [value, value] pairs instead of [key, value] pairs.
+// This make Sets compatible with maps:
+// for (const item of str.entries()) {
+//     console.log(item);
+// }
+
+// typeof in set:
+console.log(typeof str);
+
+
+// innstanceof in set:
+console.log(str instanceof Set);
+
+
